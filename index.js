99ba5915444f903579fdf59ef3b8acc171e039d8
sadiqabbasi <61027168+sadiqabbasi@users.noreply.github.com>
2022-03-31 11:49:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+// switch case
+
+// let a=15;
+
+// switch (a){
+//     case(10):
+//         console.log("value is 10");
+//         break;//breaks the flow
+//     case 15:
+//         console.log("value is 15");
+//         break;
+//         default:
+//             console.log("default case");
+//          break;
+
+// }
+
+
+// switch (true){//whenever variable comes inside the case then we use true
+//     case (a>0):
+//         console.log("+");
+//     break;
+//         case (a<0):
+//          console.log("-");
+//     break;
+//     default:
+//         console.log("equal");
+//         break;
+
+// }
+
+
+// //for loops
+
+//     let i=0;
+// for(  ; i<=10;  ){
+//     console.log("value of i is ",i);
+//     i++;
+// }
+
+
+
+// //while loop
+// //we must write thecondition properly
+// //at the end of loop we have to give incriment or decrement
+// let i=0
+// while (i<10){
+//     console.log("value of i is",i);
+//      i++;
+// }
+// console.log("outside loop");
+
+
+
+// //do while
+// var i=0;
+// do{
+//  console.log("value of i is",i);
+// i++
+
+// }while(i<10)
+
+
+// let i=0;
+// do{
+//     console.log("value of i is",i);
+//     i++; 
+// }while(i<20)
+
+
+// //nested loops
+// //for one iteration of i j will run 
+
+// for (i=0;i<3;i++){
+//     for(let j=0;j<2;j++){
+//         console.log("value of i is:",i," and j is :",j);
+
+//     }
+// }
+
+
+// //functions
+// let a=10;
+// let b=20;
+
+
+// let addition=a+b;
+// console.log(addition);
+
+
+// function add(){
+//     console.log(a+b);
+// }
+// add(22,4);
+// add(24,98);
+
+// //arguments value which we pass value are arguments
+// //value which function uses to do operation
+
+
+// //function with return
+// function addNumbers(x,y){
+//     return x+y;
+// }
+// const result =addNumbers(12,12);
+// console.log(result);
+
+
+// //function which exist only in js
+
+
+
+// for(var i=1; i<=5; i++){
+//     console.log("*" .repeat(i));
+//  }
